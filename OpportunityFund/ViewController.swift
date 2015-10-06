a722fcb7c997f9cdc7e3467eeaf0fe7c5fd55c7f
Sudhir Nakhwa <snakhwa@l-sb833mdrvg-m.homeoffice.wal-mart.com>
2015-10-06 16:01:10 -0700

    Initial Commit

---
+++ OpportunityFund/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpportunityFund
+//
+//  Created by Sudhir Nakhwa on 10/6/15.
+//  Copyright © 2015 Sudhir Nakhwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
